0904e004d2b3aa6aab250d9674c20c245e8549f1
Isaque <isaque.elis.escola@gmail.com>
2023-07-22 20:36:40 -0300

    Script para subir um servidor web no linux

---
+++ script.sh
@@ -0,0 +1,7 @@
+apt update & apt upgrade -y
+apt install apache2 -y
+apt install unzip -y
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cp -R -f linux-site-dio-main/* /var/www/html/
